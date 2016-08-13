63c6691e8d5ea036b62adb86507730f098981ca0
Sharigar <alex.akulich@gmail.com>
2016-08-13 22:26:10 +0000

    it works! refactoring is needed

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
-var express = require("express");
-var app = express();
+var http = require('http');
+var url = require('url');
+var sugar = require('sugar/date');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var server = new http.Server(function(req, res) {
+
+  var urlParsed = url.parse(req.url, true);
+  var pathDate = urlParsed.pathname.substring(1).replace(/%20/g," ");
+  
+  var sugarDate = sugar.Date.create(pathDate);
+  
+  var unix = null;
+  var dateString = null;
+  
+  if (!isNaN(sugarDate.getTime())) {
+    dateString = sugar.Date.format(sugarDate, '{medium}');
+    unix = sugarDate.getTime()/1000>>0;
+  } else {
+    var unixDate = +pathDate;
+    if (!isNaN(unixDate)) {
+      unix = unixDate
+      dateString = sugar.Date.format(new Date(unixDate), '{medium}');
+    }
+  }
+  
+  res.setHeader('Cache-control', 'no-cache,no-store,must-revalidate');
+  res.end(JSON.stringify({ "unix": unix, "natural": dateString }));
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
-});
+server.listen(8080);
